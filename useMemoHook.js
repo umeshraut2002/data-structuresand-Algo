84624091699daee78b17df64050775f2fc04aee8
umesh raut <uraut6044@gmail.com>
2024-10-13 12:19:49 +0530

    large memo use

---
+++ useMemoHook.js
@@ -0,0 +1,23 @@
+// create a react js useMemo hook functionality working same in vanilla javascript 
+
+function useMemo(arr) {
+
+}
+
+const arr = [
+    {
+        id: 1,
+        name: "umesh",
+        salary: 500000,
+        location: "hyedrabad"
+    },
+
+    { id: 2, 
+    name: , 
+    salary: , 
+    location: },
+    { id: , name: , salary: , location: },
+    { id: , name: , salary: , location: },
+    { id: , name: , salary: , location: },
+    { id: , name: , salary: , location: }
+]
