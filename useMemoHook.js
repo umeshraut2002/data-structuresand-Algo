cd92f15062448c441244071b370f3ca534e2d323
umesh raut <uraut6044@gmail.com>
2024-10-14 09:44:24 +0530

    useMemo

--- useMemoHook.js
+++ useMemoHook.js
@@ -1,7 +1,24 @@
 // create a react js useMemo hook functionality working same in vanilla javascript 
 
-function useMemo(arr) {
+function dataFun(arr) {
+    return arr.map(item => item);
+}
 
+const useMemo = (dataFun) => {
+    let cache = {};
+    return function(...args) {
+        let n = args.JSON.stringify
+
+        if (n in cache[n]) {
+            return cache[n];
+        }
+        else {
+            let res = n;
+            cache[n] = res;
+
+            return res;
+        }
+    }
 }
 
 const arr = [
@@ -12,12 +29,45 @@ const arr = [
         location: "hyedrabad"
     },
 
-    { id: 2, 
-    name: , 
-    salary: , 
-    location: },
-    { id: , name: , salary: , location: },
-    { id: , name: , salary: , location: },
-    { id: , name: , salary: , location: },
-    { id: , name: , salary: , location: }
-]
+    {
+        id: 2,
+        name: "vihaan",
+        salary: 100000,
+        location: "Mumbai"
+    },
+
+    {
+        id: 3,
+        name: "urvi",
+        salary: 1500000,
+        location: "delhi"
+    },
+
+    {
+        id: 4,
+        name: "Scala",
+        salary: 3000000,
+        location: "Silicon Valley, London"
+    },
+
+    {
+        id: 5,
+        name: "Stevian Martin",
+        salary: 900000,
+        location: "New York"
+    },
+
+    {
+        id: 6,
+        name: "Pranita",
+        salary: 1500000,
+        location: "Pune"
+    }
+];
+
+
+const memo = useMemo(dataFun);
+
+console.time();
+console.log(memo(arr.id))
+console.timeEnd();
